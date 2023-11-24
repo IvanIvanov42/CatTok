afc84c416cacaf8dcc5334f439154dcb713f0bc4
Ivan <ivan.vivanov1998@gmail.com>
2023-11-24 14:43:58 +0200

    Move Media class in its own file

---
+++ Expatery-API/Models/Media.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expatery_API.Models
+{
+    public class Media
+    {
+        public string id { get; set; }
+        public string media_type { get; set; }
+        public string media_url { get; set; }
+        public string? caption { get; set; }
+        public string username { get; set; }
+        [Column("time_stamp")]
+        public string timestamp { get; set; }
+    }
+}
