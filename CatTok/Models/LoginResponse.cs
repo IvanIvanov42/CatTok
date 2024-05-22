e9f7e8c1636cb057fa2c8f0d0cf91e7a4a54a4f6
i427232 <i427232@fontysict.nl>
2024-05-22 16:17:06 +0200

    Fix split folders due to case sensitivity

--- CatTok/Models/LoginResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CatTok.Models
-{
-    public class LoginResponse
-    {
-        public required string JwtToken { get; set; }
-        public DateTime Expiration { get; set; }
-        public required string RefreshToken { get; set; }
-    }
-}
--- CatTok/Models/RefreshModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CatTok.Models
-{
-    public class RefreshModel
-    {
-        public required string AccessToken { get; set; }
-        public required string RefreshToken { get; set; }
-    }
-}
