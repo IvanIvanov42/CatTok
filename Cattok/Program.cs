c1edc3971163b051ef7cb2a45db6e5e7006200cf
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-05-22 15:05:47 +0200

    Convert app to WASM

--- Cattok/Program.cs
+++ Cattok/Program.cs
@@ -1,41 +1,28 @@
-using CatTok.Components;
 using Blazored.Modal;
 using CatTok.Services.IServices;
 using CatTok.Services;
+using Blazored.LocalStorage;
+using CatTok.Handlers;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CatTok;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents()
-    .AddInteractiveServerComponents();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredModal();
 
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.secret.json", optional: true)
-    .Build();
+builder.Services.AddTransient<AuthenticationHandler>();
 
-builder.Services.AddHttpClient("CatTokAPI", client => client.BaseAddress = new Uri(configuration["CatTokAPIUri"]));
+builder.Services.AddHttpClient("CatTokAPI")
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.Configuration["ServerUrl"] ?? ""))
+                .AddHttpMessageHandler<AuthenticationHandler>();
 
+builder.Services.AddScoped<AuthenticationState>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<IInstagramService, InstagramService>();
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+builder.Services.AddBlazoredLocalStorage();
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
