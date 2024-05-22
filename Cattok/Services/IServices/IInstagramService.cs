ba17d16c1d8570df3d7367ae95e7a33904f683f8
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-05-22 15:05:02 +0200

    Get & display instagram media

--- Cattok/Services/IServices/IInstagramService.cs
+++ Cattok/Services/IServices/IInstagramService.cs
@@ -1,9 +1,11 @@
-﻿namespace CatTok.Services.IServices
+﻿using CatTok.Models;
+
+namespace CatTok.Services.IServices
 {
     public interface IInstagramService
     {
         Task<HttpResponseMessage> SendAuthorizationToken(string token);
-
         string GetUser();
+        Task<IEnumerable<Media>?> GetMediasAsync();
     }
 }
