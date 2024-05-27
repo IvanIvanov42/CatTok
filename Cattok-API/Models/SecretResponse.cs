4f053ab400b343dac6580a231acb05a59dc90414
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-05-27 15:30:47 +0200

    Get instagram related secrets from keyvault in API

---
+++ Cattok-API/Models/SecretResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Cattok_API.Models
+{
+    public class SecretResponse
+    {
+        public string ClientId { get; set; }
+        public string RedirectUri { get; set; }
+    }
+}
--- Cattok/Program.cs
+++ Cattok/Program.cs
@@ -6,8 +6,6 @@ using CatTok.Handlers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CatTok;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,11 +13,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredModal();
 builder.Services.AddBlazoredLocalStorage();
-
-var keyVaultUrl = builder.Configuration["KeyVault:VaultUri"];
-var credential = new DefaultAzureCredential();
-
-builder.Services.AddSingleton(x => new SecretClient(new Uri(keyVaultUrl), credential));
 
 builder.Services.AddTransient<AuthenticationHandler>();
 
