debc11234f9a572b527e4346e7602067f4b16e00
IvanIvanov42 <33684735+IvanIvanov42@users.noreply.github.com>
2024-08-29 20:42:54 +0300

    Enable backend for different users

--- Cattok-API/Data/Repository/IMediaRepository.cs
+++ Cattok-API/Data/Repository/IMediaRepository.cs
@@ -4,7 +4,7 @@ namespace Cattok_API.Data.Repository
 {
     public interface IMediaRepository
     {
-        Task<List<Media>> GetMediaListAsync();
-        Task AddMediaAsync(List<Media> mediaList);
+        Task<List<Media>> GetMediaListAsync(string userId);
+        Task AddMediaAsync(string userId, List<Media> mediaList);
     }
 }
