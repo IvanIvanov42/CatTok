44ea01913f4e56c3e8eebf05d695ebce69e7daa8
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-09-12 22:43:39 +0300

    Frontend services work with individual user media

---
+++ Cattok/Models/InstagramUser.cs
@@ -0,0 +1,14 @@
+﻿namespace CatTok.Models
+{
+    public class InstagramUser
+    {
+        public string Id { get; set; }
+        public string? InstagramUsername { get; set; }
+        public List<Media> Medias { get; set; } = new List<Media>();
+    }
+
+    public class InstagramConnectionStatus
+    {
+        public bool IsConnected { get; set; }
+    }
+}
--- Cattok/Services/InstagramService.cs
+++ Cattok/Services/InstagramService.cs
@@ -1,5 +1,7 @@
-﻿using CatTok.Models;
+﻿using Blazored.LocalStorage;
+using CatTok.Models;
 using CatTok.Services.IServices;
+using System.Data;
 using System.Net.Http.Json;
 
 namespace CatTok.Services
@@ -7,10 +9,13 @@ namespace CatTok.Services
     public class InstagramService : IInstagramService
     {
         private readonly HttpClient _httpClient;
-        
-        public InstagramService(IHttpClientFactory clientFactory)
+        private readonly ILocalStorageService _localStorage;
+
+        public InstagramService(IHttpClientFactory clientFactory, ILocalStorageService localStorage)
         {
-            _httpClient = clientFactory.CreateClient("CatTokAPI"); ;
+            _httpClient = clientFactory.CreateClient("CatTokAPI");
+            _localStorage = localStorage;
+
         }
 
         public async Task<IEnumerable<Media>?> GetMediasAsync()
@@ -19,16 +24,47 @@ namespace CatTok.Services
             return allMedias?.Where(media => media.MediaType == "IMAGE");
         }
 
-
-        public string GetUser()
+        public async Task<IEnumerable<InstagramUser>> GetUsersWithMediaAsync()
         {
-            throw new NotImplementedException();
+            var users = await _httpClient.GetFromJsonAsync<List<InstagramUser>>("api/Instagram/GetUsersMedia");
+            if (users != null && users.Count > 0)
+            {
+                return users;
+            }
+            return new List<InstagramUser>();
         }
+
 
         public async Task<HttpResponseMessage> SendAuthorizationToken(string token)
         {
             var response = await _httpClient.PostAsJsonAsync("api/Instagram/AuthorizeUser", token);
             return response;
+        }
+
+        public async Task<HttpResponseMessage> PostInstagramData()
+        {
+            var response = await _httpClient.PostAsync("api/Instagram/PostInstagramData", null);
+            return response;
+        }
+
+        public async Task<bool> IsUserConnected()
+        {
+            var cachedStatus = await _localStorage.GetItemAsync<bool?>("isInstagramConnected");
+            if (cachedStatus.HasValue)
+            {
+                return cachedStatus.Value;
+            }
+
+            var response = await _httpClient.GetAsync("api/Instagram/IsConnected");
+            if (response.IsSuccessStatusCode)
+            {
+                var connectionStatus = await response.Content.ReadFromJsonAsync<InstagramConnectionStatus>();
+                var isConnected = connectionStatus.IsConnected;
+                await _localStorage.SetItemAsync("isInstagramConnected", isConnected);
+                return isConnected;
+            }
+
+            return false;
         }
     }
 }
