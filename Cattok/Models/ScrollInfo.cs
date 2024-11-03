49ad4ef0321f7517efde02e7d882ce623c430ed6
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-11-03 22:36:24 +0200

    New scrolling logic & moved to MainLayout

---
+++ Cattok/Models/ScrollInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace CatTok.Models
+{
+    public class ScrollInfo
+    {
+        public double scrollTop { get; set; }
+        public double scrollHeight { get; set; }
+        public double clientHeight { get; set; }
+    }
+}
