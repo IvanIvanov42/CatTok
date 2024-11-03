0dcdb95266920ffb52c5a82b780280fb2c987b78
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-11-03 22:30:53 +0200

    Move loading users logic to a service

---
+++ Cattok/Services/IServices/IUserScrollingService.cs
@@ -0,0 +1,17 @@
+﻿using CatTok.Models;
+
+namespace CatTok.Services.IServices
+{
+    public interface IUserScrollingService
+    {
+        bool HasNextUser { get; }
+        bool HasPreviousUser { get; }
+        InstagramUser? SelectedUser { get; }
+
+        event Action? OnUserChanged;
+
+        Task InitializeAsync();
+        bool LoadNextUser();
+        bool LoadPreviousUser();
+    }
+}
