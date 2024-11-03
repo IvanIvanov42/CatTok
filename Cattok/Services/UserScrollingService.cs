c457b800a4db560f6563a57fe6a1382e9c80b501
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-11-03 22:30:53 +0200

    Move loading users logic to a service

---
+++ Cattok/Services/UserScrollingService.cs
@@ -0,0 +1,57 @@
+﻿using CatTok.Models;
+using CatTok.Services.IServices;
+
+namespace CatTok.Services
+{
+    public class UserScrollingService : IUserScrollingService
+    {
+        public event Action? OnUserChanged;
+        private List<InstagramUser>? Users;
+        private int CurrentUserIndex = 0;
+        public InstagramUser? SelectedUser { get; private set; }
+        public bool HasNextUser => Users != null && CurrentUserIndex < Users.Count - 1;
+        public bool HasPreviousUser => Users != null && CurrentUserIndex > 0;
+
+        private readonly IInstagramService _instagramService;
+
+        public UserScrollingService(IInstagramService instagramService)
+        {
+            _instagramService = instagramService;
+        }
+
+        public async Task InitializeAsync()
+        {
+            var users = await _instagramService.GetUsersWithMediaAsync();
+            if (users != null && users.Any())
+            {
+                Users = users.ToList();
+                SelectedUser = Users[CurrentUserIndex];
+                OnUserChanged?.Invoke();
+            }
+        }
+
+        public bool LoadNextUser()
+        {
+            if (HasNextUser)
+            {
+                CurrentUserIndex++;
+                SelectedUser = Users[CurrentUserIndex];
+                OnUserChanged?.Invoke();
+                return true;
+            }
+            return false;
+        }
+
+        public bool LoadPreviousUser()
+        {
+            if (HasPreviousUser)
+            {
+                CurrentUserIndex--;
+                SelectedUser = Users[CurrentUserIndex];
+                OnUserChanged?.Invoke();
+                return true;
+            }
+            return false;
+        }
+    }
+}
