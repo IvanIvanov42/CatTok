a93840edee57c711ed60c962657170dfdc8d3453
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-11-03 22:16:05 +0200

    Always stream muted

--- Cattok/Services/IServices/IStreamingService.cs
+++ Cattok/Services/IServices/IStreamingService.cs
@@ -6,7 +6,7 @@
         string CurrentStreamerId { get; }
 
         Task InitializeAsync();
-        Task StartStreaming(string userId, bool audio);
+        Task StartStreaming(string userId);
         Task StopStreaming(string userId);
 
         Task<List<string>> GetActiveStreamsAsync();
