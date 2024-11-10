f983202004dd11542479410971eea8428b093f5c
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-11-10 15:56:02 +0200

    Detect if device is mobile & add a livestremaing modal for mobile devices. Slight styling changes for columns of pictures

--- Cattok/wwwroot/js/userScrolling.js
+++ Cattok/wwwroot/js/userScrolling.js
@@ -13,3 +13,7 @@ window.scrollToTop = function (element) {
 window.scrollToBottom = function (element) {
     element.scrollTop = element.scrollHeight - element.clientHeight;
 };
+
+window.isMobileDevice = function () {
+    return /Android|Mobile|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
+};
