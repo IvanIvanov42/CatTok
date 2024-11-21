561b56fac1ceb9526aab4b97a815f50d9e1553e6
Ivan Ivanov <ivan.vivanov1998@gmail.com>
2024-11-21 17:16:50 +0200

    Mobile user scrolling

--- Cattok/wwwroot/js/userScrolling.js
+++ Cattok/wwwroot/js/userScrolling.js
@@ -1,3 +1,43 @@
+window.initializeInteractionListeners = function (dotNetHelper) {
+    let touchStartY = 0;
+    let touchEndY = 0;
+
+    function handleTouchStart(event) {
+        touchStartY = event.changedTouches[0].screenY;
+    }
+
+    function handleTouchEnd(event) {
+        touchEndY = event.changedTouches[0].screenY;
+        handleGesture();
+    }
+
+    function handleGesture() {
+        const deltaY = touchEndY - touchStartY;
+        if (Math.abs(deltaY) > 50) {
+            if (deltaY < 0) {
+                dotNetHelper.invokeMethodAsync('OnSwipeUp');
+            } else {
+                dotNetHelper.invokeMethodAsync('OnSwipeDown');
+            }
+        }
+    }
+
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchend', handleTouchEnd, false);
+
+    window._touchStartHandler = handleTouchStart;
+    window._touchEndHandler = handleTouchEnd;
+};
+
+window.removeInteractionListeners = function () {
+    if (window._touchStartHandler && window._touchEndHandler) {
+        document.removeEventListener('touchstart', window._touchStartHandler);
+        document.removeEventListener('touchend', window._touchEndHandler);
+        window._touchStartHandler = null;
+        window._touchEndHandler = null;
+    }
+};
+
 window.getScrollInfo = function (element) {
     return {
         scrollTop: element.scrollTop,
